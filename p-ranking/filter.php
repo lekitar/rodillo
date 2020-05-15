4fdf44366a0f9039162b0252192ed58ee63e7208
Julio Lovera <lekitar123@gmail.com>
2020-05-15 17:52:40 -0400

    filtros
    se agregan los filtros de los tabs

---
+++ p-ranking/filter.php
@@ -0,0 +1,30 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 filter-tabs">
+			<div class="row">	
+				<div class="col-md-4"> 
+					<div class="row">				
+						<div class="col-md-6">	
+							<select class="form-control">
+								<option>Pais </option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5</option>
+							</select>
+						</div>
+						<div class="col-md-6">
+							<select class="form-control">
+								<option>Continente</option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- ranking.php
+++ ranking.php
@@ -1,5 +1,6 @@
 <?php include('header.php');?> 
 <?php include('p-secctions/menu.php');?>
 <?php include('p-ranking/tabs.php');?>
+<?php include('p-ranking/filter.php');?>
 <?php include('p-ranking/tabla-ranking.php');?>
 <?php include('footer.php');?>
